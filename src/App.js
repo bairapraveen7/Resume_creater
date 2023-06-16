cc7ee02a9a350a160312c85634f2b2fe3bebe57a
Hicul <hicul@Hiculs-Air.bbrouter>
2023-06-16 23:43:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import View from './pages/View';
+import Create from './pages/Create';
+import Edit from './pages/Edit';
+import Auth from './pages/Auth';
+import { useState } from 'react';
 
 function App() {
+
+  const [ogd,setogd] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       <BrowserRouter>
+       <Header />
+      <Routes>
+        <Route path="/" element={<Home  />} />
+          <Route path="create" element={<Create originald = {ogd} setogd = {setogd}/>}/>
+          <Route path="view" element={<View originald = {ogd} setogd = {setogd} />} />
+          <Route path="edit" element={<Edit originald = {ogd} setogd = {setogd} />} />
+          <Route path = "authorize" element = {<Auth originald = {ogd} />}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
